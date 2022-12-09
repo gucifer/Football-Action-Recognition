2134bc0683c73bf8653dcfc9000473dc6973f41e
Saurav Suman <saurav.prakash.suman@outlook.in>
2022-12-09 14:20:41 +0400

    added accidentally deleted files and resolved all conflicts

--- data/srv_feat_extractor.py
+++
@@ -1,5 +0,0 @@
-import os
-if __name__ == "__main__":
-    all_game_frames_path = "/Users/srv/Documents/Cloud/Georgia Institute of Technology/Alphas - Documents/Project Data/frames"
-    feature_path = "/Users/srv/Documents/Cloud/Georgia Institute of Technology/Alphas - Documents/Project Data/vit_features_test_igore"
-    os.system(f"python -u feature_extractor_main.py -f {all_game_frames_path} -s {feature_path}")
