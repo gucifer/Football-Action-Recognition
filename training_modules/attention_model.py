ec00e7207e9ef370b1e7c1a24f76cfeb3fdc6e9a
Saurav Suman <saurav.prakash.suman@outlook.in>
2022-12-11 01:11:00 +0400

    modified args

--- training_modules/attention_model.py
+++ training_modules/attention_model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from netvlad import NetVLAD
+from .netvlad import NetVLAD
 
 
 class AttentionModel(nn.Module):
--- training_modules/model.py
+++ training_modules/model.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-from netvlad import NetVLAD, NetRVLAD
+from .netvlad import NetVLAD, NetRVLAD
 
 
 
